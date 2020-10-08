f9f9771cf627446ef0f53e0f25e8e968e4ef4302
Daan de Waard <daan.de.waard@hz.nl>
2020-10-08 10:19:37 +0200

    Initial commit

---
+++ src/StartView.ts
@@ -0,0 +1,46 @@
+/// <reference path="framework/View.ts"/>
+
+class StartView extends View {
+
+    private shouldGoToNextView: boolean = false;
+
+    private buttonImage: HTMLImageElement;
+
+    /**
+     * Let the view initialize itself within the game. This method is called
+     * once before the first game cycle.
+     * 
+     * @param {HTMLCanvasElement} canvas
+     * @param {ResourceRepository} repo
+     */
+    public init(game: Game) 
+    {
+        super.init(game);
+        this.buttonImage = game.repo.getImage("PNG.UI.buttonBlue");
+    }
+
+    public listen(input: Input) {
+        super.listen(input);
+
+        // See if user wants to go to the next screen
+        if (input.keyboard.isKeyDown(Input.KEY_S)) {
+            this.shouldGoToNextView = true;
+        }
+    }
+
+    public adjust(game: Game) {
+        if (this.shouldGoToNextView) {
+            game.switchViewTo('level');
+        }
+    }
+
+    public draw(ctx: CanvasRenderingContext2D) {
+        this.writeTextToCanvas(ctx, "Hello World!", 140, this.center.x, 150);
+
+        this.writeTextToCanvas(ctx, "PRESS PLAY OR HIT 'S' TO START", 40, this.center.x, this.center.y - 135);
+
+        this.drawImage(ctx, this.buttonImage, this.center.x, this.center.y + 220);
+        this.writeTextToCanvas(ctx, "Play", 20, this.center.x, this.center.y + 229, 'center', 'black');
+    }
+
+}
